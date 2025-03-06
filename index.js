bd9973d0afccab03d918aef37a9555d07a81eb6c
Peter kanyori <peterynaenah23@gmail.com>
2025-03-06 15:39:32 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+
+const random = Math.ceil(Math.random() * 10) + 1;
+// This will log a random number between 1 and 10
+
+let num3 = 14
+let num4 = 5
+let mod = num3 % num4
+let max = Math.max(15,16,17,18,19,20)
